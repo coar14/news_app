75699c547073fa6382933aaeeb865fda44d81525
coar14 <m.masong14@gmail.com>
2023-10-27 20:59:57 +0800

    Text inside a Column for welcome msgs and currentDate

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:news_app/const_style.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    String currentDate = DateFormat('EEEE, d MMMM').format(DateTime.now());
+
     return SafeArea(
       child: ListView(
         padding: const EdgeInsets.symmetric(
+          vertical: 56,
           horizontal: 30,
         ),
         children: [
@@ -19,14 +24,40 @@ class HomePage extends StatelessWidget {
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(16),
                   image: const DecorationImage(
-                    image: AssetImage(
-                      'assets/avatar/male.png',
-                    ),
+                    image: AssetImage('assets/avatar/male.png'),
                   ),
                 ),
-              )
+              ),
+              const SizedBox(
+                width: 16,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Welcome Back!',
+                    style: TextStyle(
+                      fontFamily: 'Gellix',
+                      fontWeight: FontWeight.w700,
+                      color: Colors.black,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 7,
+                  ),
+                  Text(
+                    currentDate,
+                    style: TextStyle(
+                      fontFamily: 'Gellix',
+                      fontWeight: FontWeight.w300,
+                      color: aLightGray,
+                    ),
+                  ),
+                ],
+              ),
             ],
-          )
+          ),
         ],
       ),
     );
