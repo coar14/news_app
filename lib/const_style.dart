2d357541b37fb035536a3ab6f11f4bdd24510c9c
coar14 <m.masong14@gmail.com>
2023-10-27 20:21:34 +0800

    Initial commit

---
+++ lib/const_style.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+const Color aWhite = Color(0xffffffff);
+const Color aLightWhite = Color(0xffeff5f4);
+const Color aLighterWhite = Color(0xfffcfcfc);
+const Color aGray = Color(0xff9397a0);
+const Color aLightGray = Color(0xffa7a7a7);
+const Color aBlue = Color(0xff5474fd);
+const Color aLightBlue = Color(0xff83b1ff);
+const Color aLighterBlue = Color(0xffc1d4f9);
+const Color aDarkBlue = Color(0xff19202d);
+
+const fGellix = (
+  fontFamily: 'Gellix',
+  fontWeight: FontWeight.w700,
+);
+
+const fGellixSemiBold = (
+  fontFamily: 'Gellix',
+  fontWeight: FontWeight.w600,
+);
+const fGellixMedium = (
+  fontFamily: 'Gellix',
+  fontWeight: FontWeight.w500,
+);
+const fGellixRegular = (
+  fontFamily: 'Gellix',
+  fontWeight: FontWeight.w400,
+);
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'const_style.dart';
+import 'package:news_app/pages/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
