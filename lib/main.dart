839c5229ffc974d1340d9c10b9af59046fbb1c24
coar14 <m.masong14@gmail.com>
2023-10-27 20:34:01 +0800

    stateful widget for main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,11 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          backgroundColor: aLightWhite,
+          body: HomePage(),
+        ));
   }
 }
