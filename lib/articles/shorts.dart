a97a4c11ac25cb6f0055cb78e0568fb5c42e4c5f
coar14 <m.masong14@gmail.com>
2023-10-28 23:11:50 +0800

    a class for Shorts Sections

---
+++ lib/articles/shorts.dart
@@ -0,0 +1,31 @@
+class ShortForYou {
+  ShortForYou({
+    this.photo = ' ',
+    this.heading = ' ',
+    this.views = ' ',
+  });
+  String photo;
+  String heading;
+  String views;
+}
+
+var shortForYou = [
+  ShortForYou(
+      photo: 'assets/pic/a.jpg',
+      heading: 'Explore The World: Explore Korea',
+      views: '2,478'),
+  ShortForYou(
+      photo: 'assets/pic/b.jpg', heading: 'Japan 24/7 Street View', views: '11,069'),
+  ShortForYou(
+      photo: 'assets/pic/c.jpg',
+      heading: 'Keeping Calm while Traveling',
+      views: '5,298'),
+  ShortForYou(
+      photo: 'assets/pic/d.jpg',
+      heading: 'Philippines: Love of Nation',
+      views: '41,024'),
+  ShortForYou(
+      photo: 'assets/pic/e.jpg',
+      heading: 'Surfing Like No Tomorrow',
+      views: '321,002'),
+];
