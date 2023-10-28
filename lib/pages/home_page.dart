cd214cb61f37a34e30496e0170c96fe3e740793c
coar14 <m.masong14@gmail.com>
2023-10-28 23:20:45 +0800

    return safeArea for home screen

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:intl/intl.dart';
+import 'package:news_app/components/article_list.dart';
+
+import 'package:news_app/components/search_tags.dart';
+import 'package:news_app/components/shorts_section.dart';
 import 'package:news_app/const_style.dart';
 
 class HomePage extends StatelessWidget {
@@ -13,7 +17,7 @@ class HomePage extends StatelessWidget {
     return SafeArea(
       child: ListView(
         padding: const EdgeInsets.symmetric(
-          vertical: 56,
+          vertical: 35,
           horizontal: 30,
         ),
         children: [
@@ -25,7 +29,7 @@ class HomePage extends StatelessWidget {
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(16),
                   image: const DecorationImage(
-                    image: AssetImage('assets/avatar/male.png'),
+                    image: AssetImage('assets/pic/dp4.jpg'),
                   ),
                 ),
               ),
@@ -36,7 +40,7 @@ class HomePage extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text(
+                  const Text(
                     'Welcome Back!',
                     style: TextStyle(
                       fontFamily: 'Gellix',
@@ -44,12 +48,12 @@ class HomePage extends StatelessWidget {
                       color: Colors.black,
                     ),
                   ),
-                  SizedBox(
+                  const SizedBox(
                     height: 7,
                   ),
                   Text(
                     currentDate,
-                    style: TextStyle(
+                    style: const TextStyle(
                       fontFamily: 'Gellix',
                       fontWeight: FontWeight.w300,
                       color: aLightGray,
@@ -63,49 +67,61 @@ class HomePage extends StatelessWidget {
             height: 30,
           ),
           Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(borderRad),
-                boxShadow: [
-                  BoxShadow(
-                    color: aDarkBlue.withOpacity(0.052),
-                    offset: const Offset(0.0, 3.0),
-                    blurRadius: 25,
-                    spreadRadius: 0.0,
-                  ),
-                ],
-              ),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      style: TextStyle(
-                        fontFamily: 'Gellix',
-                        fontSize: 12,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(borderRad),
+              boxShadow: [
+                BoxShadow(
+                  color: aDarkBlue.withOpacity(0.052),
+                  offset: const Offset(0.0, 3.0),
+                  blurRadius: 25,
+                  spreadRadius: 0.0,
+                ),
+              ],
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    style: const TextStyle(
+                      fontFamily: 'Gellix',
+                      fontSize: 12,
+                    ),
+                    decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.symmetric(
+                        horizontal: 20,
                       ),
-                      decoration: InputDecoration(
-                        contentPadding: const EdgeInsets.symmetric(
-                          horizontal: 20,
-                        ),
-                        hintText: 'Search for article...',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(16.0),
-                          borderSide: BorderSide.none,
-                        ),
+                      hintText: 'Search for article...',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16.0),
+                        borderSide: BorderSide.none,
                       ),
                     ),
                   ),
-                  Container(
-                    decoration: BoxDecoration(
-                        color: aBlue,
-                        borderRadius: BorderRadius.circular(16.0)),
-                    child: IconButton(
-                      onPressed: () {},
-                      icon: SvgPicture.asset('assets/icons/search_icon.svg'),
-                    ),
-                  )
-                ],
-              ))
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      color: aBlue, borderRadius: BorderRadius.circular(16.0)),
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: SvgPicture.asset('assets/icons/search_icon.svg'),
+                  ),
+                )
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          TagLists(),
+          const SizedBox(
+            height: 30,
+          ),
+          const HorizontalListArticle(),
+          const SizedBox(
+            height: 30,
+          ),
+          const ShortSection(),
         ],
       ),
     );
