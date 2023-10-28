decb4c2b459ad8b8f7b2d8c8797d16495cd140ad
coar14 <m.masong14@gmail.com>
2023-10-28 23:10:43 +0800

    added an indexedStack to route particular screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/components/nav_bar.dart';
+import 'package:news_app/screens/news_detail_screen.dart';
+import 'package:news_app/screens/notif_screen.dart';
+import 'package:news_app/screens/user_profile_screen.dart';
 import 'const_style.dart';
 import 'package:news_app/pages/home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const MyApp(),
+  );
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
+  int selectedIndex = 0;
+
+  void onItemTapped(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          backgroundColor: aLighterWhite,
-          body: HomePage(),
-        ));
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: aWhite,
+        body: IndexedStack(
+          index: selectedIndex,
+          children: const [
+            HomePage(),
+            NewsDetailScreen(),
+            NotifScreen(),
+            UserProfile(),
+          ],
+        ),
+        bottomNavigationBar: NavBar(
+          selectedIndex: selectedIndex,
+          onItemTapped: onItemTapped,
+        ),
+      ),
+    );
   }
 }
