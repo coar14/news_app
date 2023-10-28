7a461466b69062840a34916c219f98c6b9d21852
coar14 <m.masong14@gmail.com>
2023-10-28 23:10:03 +0800

    created sizeConfig as constant

--- lib/const_style.dart
+++ lib/const_style.dart
@@ -9,10 +9,27 @@ const Color aBlue = Color(0xff5474fd);
 const Color aLightBlue = Color(0xff83b1ff);
 const Color aLighterBlue = Color(0xffc1d4f9);
 const Color aDarkBlue = Color(0xff19202d);
-
+const Color aBlack = Color.fromARGB(0, 0, 0, 0);
+const Color aBorder = Color(0xffEEEEEE);
 const double borderRad = 16.0;
-
+const double padHorizontal = 40.0;
 final kBorder = OutlineInputBorder(
   borderRadius: BorderRadius.circular(borderRad),
   borderSide: BorderSide.none,
 );
+
+class SizeConfig {
+  static MediaQueryData? _mediaQueryData;
+  static double? screenWidth;
+  static double? screenHeight;
+  static double? blockSizeHorizontal;
+  static double? blockSizeVertical;
+
+  void init(BuildContext context) {
+    _mediaQueryData = MediaQuery.of(context);
+    screenWidth = _mediaQueryData!.size.width;
+    screenHeight = _mediaQueryData!.size.height;
+    blockSizeHorizontal = screenWidth! / 100;
+    blockSizeVertical = screenHeight! / 100;
+  }
+}
