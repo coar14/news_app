61f2fff02638b5f433a7a7b4b333bc0f37147f59
coar14 <m.masong14@gmail.com>
2023-10-28 23:12:06 +0800

    an array of tags

---
+++ lib/articles/tags.dart
@@ -0,0 +1,12 @@
+class TagsList {
+  final List<String> tagsList = [
+    '#Health',
+    '#Music',
+    '#Technology',
+    '#Sports',
+    '#Food',
+    '#Politics',
+    '#WFH',
+    '#Philippines',
+  ];
+}
